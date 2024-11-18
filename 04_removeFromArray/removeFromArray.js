95c8fd46c39ed9125815199ee404f9f112e50fdb
Helio Nunes <helioaugustofn@protonmail.com>
2024-11-18 05:12:32 -0400

    Exercises 01 ~ 05 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...values) {
+  return array.filter((val) => !values.includes(val));
 };
 
 // Do not edit below this line
